397195a217a0217799f9fd4aba292a635d145673
shyheng <shy2210278285@aliyun.com>
2022-05-12 21:38:55 +0800

    MongoDB增删改查

--- demo/src/main/java/com/example/demo/c/TC.java
+++ demo/src/main/java/com/example/demo/c/TC.java
@@ -1,6 +1,6 @@
 package com.example.demo.c;
 
-import com.example.demo.model.Test;
+import com.example.demo.model.L;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +10,29 @@ import java.util.List;
 
 @RestController
 public class TC {
-
     @Autowired
     private MongoTemplate mongoTemplate;
 
 
     @GetMapping("/s")
-    public List<Test> get(){
-        return mongoTemplate.findAll(Test.class);
+    public List<L> get(){
+        return mongoTemplate.findAll(L.class);
+    }
+
+    @GetMapping("/i")
+    public Object in(){
+        L l = new L();
+        l.setName("shy");
+        l.setPass("123");
+        return mongoTemplate.insert(l);
+    }
+
+    @GetMapping("/up")
+    public Object up(){
+        L l = new L();
+        l.setId("1");
+        l.setName("shy1");
+        l.setPass("123");
+        return mongoTemplate.save(l);
     }
 }
